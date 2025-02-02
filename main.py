f8d2113a80f3b2ecef3b8171434afbace19704c4
Everton Silva <evertom.silva@outlook.com.br>
2025-02-02 15:52:07 -0300

    save

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
-from fastapi import FastAPI # Importando a biblioteca FastAPI
+from fastapi import FastAPI, Query # Importando a biblioteca FastAPI
+import requests # Importando a biblioteca requests para realizar requisições HTTP
 
 app = FastAPI()
+
+@app.get('/api/hello')
+def hello_world():
+    '''
+    Endpoint para retornar "Hello, World!" como resposta.
+
+    '''
+    return {'Hello': 'World!'}
+
+@app.get('/api/restaurantes')
+def get_restaurantes(restaurante: str = Query(None)):
+    '''
+    
+    Endpoint para retornar os restaurantes com cardápio.
+    
+    '''
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return dados_json
+
+        dados_restaurante = []
+        for item in dados_json:
+            if item['Company'] == restaurante:
+                dados_restaurante.append({
+                    "item": item['Item'],
+                    "price": item['price'],
+                    "description": item['description']
+                })
+        return {'Restaurante':restaurante, 'Cardápio': dados_restaurante}
+
+    else:
+        return {'Error': f'Erro ao acessar a API: {response.status_code} - {response.text}!'  }
+
+
